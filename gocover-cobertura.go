2d9a2fccf2de9f9db56861e01814d20373226394
Yukinari Toyota <xxseyxx@gmail.com>
2013-10-30 22:03:17 +0900

    impl a bit.

--- gocover-cobertura.go
+++ gocover-cobertura.go
@@ -3,8 +3,12 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"go/ast"
 	"go/build"
+	"go/parser"
+	"go/token"
 	"io"
+	"io/ioutil"
 	"os"
 	"strings"
 	"time"
@@ -27,7 +31,7 @@ func convert(in io.Reader, out io.Writer) {
 	}
 
 	coverage := Coverage{Sources: sources, Packages: nil, Timestamp: time.Now().UnixNano() / int64(time.Millisecond)}
-	convertProfilesToPackages(profiles, &coverage)
+	coverage.parseProfiles(profiles)
 
 	fmt.Fprintf(out, xml.Header)
 	fmt.Fprintf(out, "<!DOCTYPE coverage SYSTEM \"http://cobertura.sourceforge.net/xml/coverage-03.dtd\">\n")
@@ -42,8 +46,101 @@ func convert(in io.Reader, out io.Writer) {
 	fmt.Fprintln(out)
 }
 
-func convertProfilesToPackages(profiles []*Profile, coverage *Coverage) {
-	coverage.Packages = []Package{}
+func (cov *Coverage) parseProfiles(profiles []*Profile) error {
+	cov.Packages = []Package{}
+	for _, profile := range profiles {
+		fn := profile.FileName
+		file, err := findFile(fn)
+		if err != nil {
+			return err
+		}
+		cov.parseFile(file)
+	}
+	return nil
+}
+
+func (cov *Coverage) parseFile(fileName string) error {
+	fset := token.NewFileSet()
+	parsed, err := parser.ParseFile(fset, fileName, nil, 0)
+	if err != nil {
+		return err
+	}
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return err
+	}
+
+	var pkg *Package
+	for _, p := range cov.Packages {
+		if p.Name == fileName {
+			pkg = &p
+		}
+	}
+	if pkg == nil {
+		pkg = &Package{Name: fileName, Classes: []Class{}}
+		cov.Packages = append(cov.Packages, *pkg)
+	}
+	visitor := &fileVisitor{
+		fset:     fset,
+		name:     fileName,
+		astFile:  parsed,
+		coverage: cov,
+		classes:  make(map[string]*Class),
+		pkg:      pkg,
+		data:     data,
+	}
+	ast.Walk(visitor, visitor.astFile)
+	return nil
+}
+
+type fileVisitor struct {
+	fset     *token.FileSet
+	name     string
+	astFile  *ast.File
+	coverage *Coverage
+	classes  map[string]*Class
+	pkg      *Package
+	data     []byte
+}
+
+func (v *fileVisitor) Visit(node ast.Node) ast.Visitor {
+	switch n := node.(type) {
+	case *ast.FuncDecl:
+		class := v.class(n)
+		method := v.method(n)
+		class.Methods = append(class.Methods, *method)
+		for _, line := range method.Lines {
+			class.Lines = append(class.Lines, line)
+		}
+		v.pkg.Classes = append(v.pkg.Classes, *class)
+	}
+	return v
+}
+
+func (v *fileVisitor) method(n *ast.FuncDecl) *Method {
+	method := &Method{Name: n.Name.Name}
+	method.Lines = []Line{}
+	return method
+}
+
+func (v *fileVisitor) class(n *ast.FuncDecl) *Class {
+	className := v.recvName(n)
+	var class *Class = v.classes[className]
+	if class == nil {
+		class = &Class{Name: className, Filename: v.name, Methods: []Method{}, Lines: []Line{}}
+		v.classes[className] = class
+	}
+	return class
+}
+
+func (v *fileVisitor) recvName(n *ast.FuncDecl) string {
+	if n.Recv == nil {
+		return "-"
+	}
+	recv := n.Recv.List[0].Type
+	start := v.fset.Position(recv.Pos())
+	end := v.fset.Position(recv.End())
+	return string(v.data[start.Offset:end.Offset])
 }
 
 func stripKnownSources(sources []Source, fileName string) string {
